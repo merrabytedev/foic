e08ef1e5b6894257feeaea1b3d026a4647168aba
merrabytedev <130908172+merrabytedev@users.noreply.github.com>
2023-11-29 15:40:43 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,108 +1,304 @@
-import { ConnectWallet } from "@thirdweb-dev/react";
-import styles from "../styles/Home.module.css";
+import {
+  useActiveClaimConditionForWallet,
+  useAddress,
+  useClaimConditions,
+  useClaimerProofs,
+  useClaimIneligibilityReasons,
+  useContract,
+  useContractMetadata,
+  useTokenSupply,
+  useTokenBalance,
+  ConnectWallet,
+  Web3Button,
+} from "@thirdweb-dev/react";
+import { BigNumber, utils } from "ethers";
 import Image from "next/image";
-import { NextPage } from "next";
+import { useMemo, useState } from "react";
+import styles from "../styles/Home.module.css";
+import { parseIneligibility } from "../utils/parseIneligibility";
 
-const Home: NextPage = () => {
+const Home = () => {
+  //const tokenAddress = "0xacEFf07f6e1FC0e7a14428599E1F00A73A9e8a50";
+  const tokenAddress = "0xacEFf07f6e1FC0e7a14428599E1F00A73A9e8a50";
+  const { contract } = useContract(tokenAddress, "token-drop");
+  const address = useAddress();
+  const [quantity, setQuantity] = useState(1);
+  const { data: contractMetadata } = useContractMetadata(contract);
+  const { data: tokenSupply } = useTokenSupply(contract);
+  const { data: claimTokens } = useClaimConditions(contract);
+  const { data: tokenBalance } = useTokenBalance(contract, address);
+
+  const claimConditions = useClaimConditions(contract);
+  const activeClaimCondition = useActiveClaimConditionForWallet(
+    contract,
+    address
+  );
+
+  const claimerProofs = useClaimerProofs(contract, address || "");
+  const claimIneligibilityReasons = useClaimIneligibilityReasons(contract, {
+    quantity,
+    walletAddress: address || "",
+  });
+
+  const claimedSupply = useTokenSupply(contract);
+
+  const totalAvailableSupply = useMemo(() => {
+    try {
+      return BigNumber.from(activeClaimCondition.data?.availableSupply || 0);
+    } catch {
+      return BigNumber.from(750_000);
+    }
+  }, [activeClaimCondition.data?.availableSupply]);
+
+  const numberClaimed = useMemo(() => {
+    return BigNumber.from(claimedSupply.data?.value || 0).toString();
+  }, [claimedSupply]);
+
+  const numberTotal = useMemo(() => {
+    const n = totalAvailableSupply.add(
+      BigNumber.from(claimedSupply.data?.value || 0)
+    );
+    if (n.gte(750_000)) {
+      return "";
+    }
+    return n.toString();
+  }, [totalAvailableSupply, claimedSupply]);
+
+  const priceToMint = useMemo(() => {
+    if (quantity) {
+      const bnPrice = BigNumber.from(
+        activeClaimCondition.data?.currencyMetadata.value || 0
+      );
+      return `${utils.formatUnits(
+        bnPrice.mul(quantity).toString(),
+        activeClaimCondition.data?.currencyMetadata.decimals || 18
+      )} ${activeClaimCondition.data?.currencyMetadata.symbol}`;
+    }
+  }, [
+    activeClaimCondition.data?.currencyMetadata.decimals,
+    activeClaimCondition.data?.currencyMetadata.symbol,
+    activeClaimCondition.data?.currencyMetadata.value,
+    quantity,
+  ]);
+
+  const maxClaimable = useMemo(() => {
+    let bnMaxClaimable;
+    try {
+      bnMaxClaimable = BigNumber.from(
+        activeClaimCondition.data?.maxClaimableSupply || 0
+      );
+    } catch (e) {
+      bnMaxClaimable = BigNumber.from(750_000);
+    }
+
+    let perTransactionClaimable;
+    try {
+      perTransactionClaimable = BigNumber.from(
+        activeClaimCondition.data?.maxClaimablePerWallet || 0
+      );
+    } catch (e) {
+      perTransactionClaimable = BigNumber.from(750_000);
+    }
+
+    if (perTransactionClaimable.lte(bnMaxClaimable)) {
+      bnMaxClaimable = perTransactionClaimable;
+    }
+
+    const snapshotClaimable = claimerProofs.data?.maxClaimable;
+
+    if (snapshotClaimable) {
+      if (snapshotClaimable === "0") {
+        // allowed unlimited for the snapshot
+        bnMaxClaimable = BigNumber.from(750_000);
+      } else {
+        try {
+          bnMaxClaimable = BigNumber.from(snapshotClaimable);
+        } catch (e) {
+          // fall back to default case
+        }
+      }
+    }
+
+    let max;
+    if (totalAvailableSupply.lt(bnMaxClaimable)) {
+      max = totalAvailableSupply;
+    } else {
+      max = bnMaxClaimable;
+    }
+
+    if (max.gte(750_000)) {
+      return 750_000;
+    }
+    return max.toNumber();
+  }, [
+    claimerProofs.data?.maxClaimable,
+    totalAvailableSupply,
+    activeClaimCondition.data?.maxClaimableSupply,
+    activeClaimCondition.data?.maxClaimablePerWallet,
+  ]);
+
+  const isSoldOut = useMemo(() => {
+    try {
+      return (
+        (activeClaimCondition.isSuccess &&
+          BigNumber.from(activeClaimCondition.data?.availableSupply || 0).lte(
+            0
+          )) ||
+        numberClaimed === numberTotal
+      );
+    } catch (e) {
+      return false;
+    }
+  }, [
+    activeClaimCondition.data?.availableSupply,
+    activeClaimCondition.isSuccess,
+    numberClaimed,
+    numberTotal,
+  ]);
+
+  const canClaim = useMemo(() => {
+    return (
+      activeClaimCondition.isSuccess &&
+      claimIneligibilityReasons.isSuccess &&
+      claimIneligibilityReasons.data?.length === 0 &&
+      !isSoldOut
+    );
+  }, [
+    activeClaimCondition.isSuccess,
+    claimIneligibilityReasons.data?.length,
+    claimIneligibilityReasons.isSuccess,
+    isSoldOut,
+  ]);
+
+  const isLoading = useMemo(() => {
+    return activeClaimCondition.isLoading || !contract;
+  }, [activeClaimCondition.isLoading, contract]);
+
+  const buttonLoading = useMemo(
+    () => isLoading || claimIneligibilityReasons.isLoading,
+    [claimIneligibilityReasons.isLoading, isLoading]
+  );
+  const buttonText = useMemo(() => {
+    if (isSoldOut) {
+      return "Sold out";
+    }
+
+    if (canClaim) {
+      const pricePerToken = BigNumber.from(
+        activeClaimCondition.data?.currencyMetadata.value || 0
+      );
+      if (pricePerToken.eq(0)) {
+        return "Mint (Free)";
+      }
+      return `TOTAL COSTS: ${priceToMint}`;
+    }
+    if (claimIneligibilityReasons.data?.length) {
+      return parseIneligibility(claimIneligibilityReasons.data, quantity);
+    }
+    if (buttonLoading) {
+      return "Fetching FOIC";
+    }
+
+    return "Claiming not available";
+  }, [
+    isSoldOut,
+    canClaim,
+    claimIneligibilityReasons.data,
+    buttonLoading,
+    activeClaimCondition.data?.currencyMetadata.value,
+    priceToMint,
+    quantity,
+  ]);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            Welcome to{" "}
-            <span className={styles.gradientText0}>
-              <a
-                href="https://thirdweb.com/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                thirdweb.
-              </a>
-            </span>
-          </h1>
 
-          <p className={styles.description}>
-            Get started by configuring your desired network in{" "}
-            <code className={styles.code}>src/index.js</code>, then modify the{" "}
-            <code className={styles.code}>src/App.js</code> file!
+
+  <div className={styles.container}>
+  <div className={styles.connect}>
+
+  <ConnectWallet
+  accentColor="#64CCCD"
+  colorMode="dark"
+  className="web3btn"
+  >
+  </ConnectWallet>
+  </div>
+  <div className={styles.balance}>
+  <p>YOUR FOIC BALANCE:</p>
+  <p>{tokenBalance?.displayValue} {tokenBalance?.symbol}</p>
+  </div>
+
+  <Image
+     src="/main.jpg"
+     alt=""
+     quality="100"
+     sizes="(max-width: 500px) 100vw,
+         (max-width: 700px) 50vw,
+         33vw"
+
+   />
+
+      {(claimConditions.data &&
+        claimConditions.data.length > 0 &&
+        activeClaimCondition.isError) ||
+        (activeClaimCondition.data &&
+          activeClaimCondition.data.startTime > new Date() && (
+            <p>Presale is starting soon.</p>
+          ))}
+
+      {claimConditions.data?.length === 0 ||
+        (claimConditions.data?.every((cc) => cc.maxClaimableSupply === "0") && (
+          <p>
+            The presale has not started yet.
           </p>
+        ))}
 
-          <div className={styles.connect}>
-            <ConnectWallet
-              dropdownPosition={{
-                side: "bottom",
-                align: "center",
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+
+        <p className={styles.explain}>
+        <p>AWESOME, YOU FOUND THIS PAGE.</p>
+          <p>BUT THIS IS A TESTPAGE. DO NOT BUY!</p>
+          <p>{activeClaimCondition.data?.availableSupply} / 115000000.0 FOIC</p>
+
+        </p>
+
+          <div className={styles.claimGrid}>
+            <input
+              type="number"
+              placeholder="Enter amount"
+              onChange={(e) => {
+                const value = parseInt(e.target.value);
+                if (value > maxClaimable) {
+                  setQuantity(maxClaimable);
+                } else if (value < 1) {
+                  setQuantity(1);
+                } else {
+                  setQuantity(value);
+                }
               }}
+              value={quantity}
+              className={`${styles.textInput} ${styles.noGapBottom}`}
             />
+            <Web3Button
+              accentColor="#64CCCD"
+              colorMode="dark"
+              contractAddress={tokenAddress}
+              action={(contract) => contract.erc20.claim(quantity)}
+              className="web3btn"
+            >
+            <p>BUY</p>
+            </Web3Button>
           </div>
-        </div>
 
-        <div className={styles.grid}>
-          <a
-            href="https://portal.thirdweb.com/"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/portal-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText1}>Portal ➜</h2>
-              <p>
-                Guides, references, and resources that will help you build with
-                thirdweb.
-              </p>
-            </div>
-          </a>
+        </>
+      )}
+      <p>{buttonText}</p>
 
-          <a
-            href="https://thirdweb.com/dashboard"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/dashboard-preview.png"
-              alt="Placeholder preview of starter"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText2}>Dashboard ➜</h2>
-              <p>
-                Deploy, configure, and manage your smart contracts from the
-                dashboard.
-              </p>
-            </div>
-          </a>
+    </div>
 
-          <a
-            href="https://thirdweb.com/templates"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/templates-preview.png"
-              alt="Placeholder preview of templates"
-              width={300}
-              height={200}
-            />
-            <div className={styles.cardText}>
-              <h2 className={styles.gradientText3}>Templates ➜</h2>
-              <p>
-                Discover and clone template projects showcasing thirdweb
-                features.
-              </p>
-            </div>
-          </a>
-        </div>
-      </div>
-    </main>
   );
 };
 
